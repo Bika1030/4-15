79f4d00db1cb79eb6cd0172ea8a40af9a3df7ef6
unknown <shybika2@gmail.com>
2024-04-15 20:33:10 +0800

    first commit

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    public function profiles(){
+        return $this->hasOne(Profile::class, 'student_id','id');
+
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class, 'student_id','id');
+    }
+
+    public function subjects(){
+        return $this->belongsToMany(Subject::class, 'student_sub');
+    }
+}
